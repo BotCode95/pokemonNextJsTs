b2e34d316cfef25c5bd93a1c46a6e92c3449e930
Patricio Bottino <patribottino95@gmail.com>
2022-03-14 21:37:35 -0300

    pokemon getStaticPaths

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,18 +1,44 @@
+import { GetStaticProps, GetStaticPaths, NextPage } from 'next'
 import { useRouter } from 'next/router'
-import React from 'react'
+import { pokeApi } from '../../api'
 import { Layout } from '../../components/layouts'
+import { Pokemon, PokemonListResponse } from '../../interfaces'
 
 interface Props {
-  pokemon: any
+  pokemon: Pokemon
 }
 
- const PokemonPage = () => {
-  const router = useRouter();
+ const PokemonPage : NextPage<Props> = ({pokemon}) => {
+  
   return (
     <Layout title='Pokemon ID'>
-      <h1>PokemonPage</h1>
+      <h1>{pokemon.id} - {pokemon.name}</h1>
     </Layout>
   )
+}
+
+
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+  //const { data } = await  // your fetch function here 
+  const pokemonsPage = [...Array(151)].map((value, index) => `${index +1}`)
+  return {
+    paths: pokemonsPage.map(id => ({
+        params: {
+          id
+        }
+
+    })),
+    fallback: false
+  }
+}
+export const getStaticProps : GetStaticProps = async({params}) => {
+  const {id} = params as {id: string}
+  const {data} = await pokeApi.get<Pokemon>(`/pokemon/${id}`);
+  return {
+    props: {
+      pokemon: data,
+    }
+  }
 }
 
 export default PokemonPage
