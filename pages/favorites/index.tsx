1ac8bcab54add31e040bd25387f70b5aa44e6e79
PBottino <pbottino@axxonconsulting.com>
2022-03-15 16:54:58 -0300

    view pokemon id and name, confetti

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
 import { Layout } from '../../components/layouts/Layout';
+import { NoFavorites } from '../../components/layouts/ui/NoFavorites';
+import { useState, useEffect } from 'react';
+import { localFavorites } from '../../utils';
+import { FavoritesPokemons } from '../../components/layouts/pokemon';
 
 const FavoritesPage = () => {
+
+  const [favoritesPokemons, setFavoritesPokemons] = useState<number[]>([])
+
+  useEffect(() => {
+    setFavoritesPokemons(localFavorites.pokemons())
+  }, [])
+  
   return (
     <Layout title='Favorites'>
-        <h1>Favorites</h1>
+      {
+        favoritesPokemons.length === 0  
+        ? <NoFavorites/> 
+        : <FavoritesPokemons pokemons={favoritesPokemons}/>
+      }
     </Layout>
   )
 }
--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,17 +1,41 @@
+import { useState } from 'react';
 import { GetStaticProps, GetStaticPaths, NextPage } from 'next'
 import { Grid, Card, Text, Button, Container, Image} from '@nextui-org/react';
+import confetti from 'canvas-confetti'
 import { pokeApi } from '../../api'
 import { Layout } from '../../components/layouts'
 import { Pokemon } from '../../interfaces'
+import { localFavorites } from '../../utils';
 
 interface Props {
   pokemon: Pokemon
 }
 
  const PokemonPage : NextPage<Props> = ({pokemon}) => {
-  console.log(pokemon.sprites.other?.dream_world.front_default)
+
+  const [isInFavorite, setIsInFavorite] = useState(localFavorites.existInFavorites(pokemon.id))
+
+  const onToggleFavorite = () => {
+    localFavorites.toggleFavorite(pokemon.id)
+    setIsInFavorite(!isInFavorite)
+
+    if(isInFavorite) return;
+
+    confetti({
+      zIndex: 999,
+      particleCount: 100,
+      spread: 160,
+      angle: -100,
+      origin: {
+        x: 1,
+        y: 0
+      }
+      
+    })
+  }
+
   return (
-    <Layout title='Pokemon ID'>
+    <Layout title={pokemon.name}>
       <Grid.Container css={{marginTop: '5px'}} gap={2}>
         <Grid xs={2} sm={4}>
           <Card hoverable css={{padding: '30px'}}>
@@ -31,8 +55,11 @@ interface Props {
               <Text h1 transform='capitalize'>{pokemon.name}</Text>
               <Button
                 color="gradient"
-                ghost
-              >Guardar en favoritos
+                ghost={!isInFavorite}
+                onClick={onToggleFavorite}
+              >{
+                isInFavorite ? 'En Favoritos' : "Guardar en favoritos"
+              }
               </Button>
 
           </Card.Header>
