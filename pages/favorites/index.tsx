8d1150f448acaf3cc0d707215b812c98390569a5
Patricio Bottino <patribottino95@gmail.com>
2022-03-15 09:05:18 -0300

    page favorites

---
+++ pages/favorites/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Layout } from '../../components/layouts/Layout';
+
+const FavoritesPage = () => {
+  return (
+    <Layout title='Favorites'>
+        <h1>Favorites</h1>
+    </Layout>
+  )
+}
+
+export default FavoritesPage
